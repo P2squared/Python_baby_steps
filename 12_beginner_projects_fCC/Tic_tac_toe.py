57241435a3ee67fe0df2b8894864be5986e1f9ea
P2squared <prathamesh_patalay@outlook.com>
2023-11-09 09:35:15 +0100

    With Minimax PC player

--- 12_beginner_projects_fCC/Tic_tac_toe.py
+++ 12_beginner_projects_fCC/Tic_tac_toe.py
@@ -1,11 +1,11 @@
 import math
 import time
-from Tic_tac_toe_player import HumPlay, RandPC
+from Tic_tac_toe_player import HumPlay, RandPC, GeniPC
 
 class TicTacToe: #all game functions
     def __init__(self):
         self.board = [' ' for _ in range(9)]
-        self.winner = None
+        self.winner = None #X or O
 
     def print_board(self): #prints board with the live contents in squares
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
@@ -29,8 +29,8 @@ class TicTacToe: #all game functions
     def make_move(self, sqr, letter): #runs action from any player and checks for victory
         if self.board[sqr] == ' ':
             self.board[sqr] = letter
-            if self.win(sqr, letter):
-                self.winner = letter
+            if self.win(sqr, letter): #if true
+                self.winner = letter #record whether X or O won
             return True
         else: return False
 
@@ -38,23 +38,23 @@ class TicTacToe: #all game functions
         row_ind = sqr //3 #find which row the last move was made in
         row = self.board[row_ind*3: (row_ind + 1)*3] #find content of all sqrs in that row
         if all([spot == letter for spot in row]) :# if every sqr in the row is the player letter
-            print('Winner by row')
+            #print('Winner by row')
             return True
 
         col_ind = sqr%3 #find column the last move was made in
         col = [self.board[col_ind+i*3] for i in range(3)] #find content of all sqrs in that column
         if all([spot == letter for spot in col]) :# if every sqr in the column is the player letter
-            print('Winner by column')
+            #print('Winner by column')
             return True
 
         if sqr % 2 == 0 :# if sqr is one of the corners or center square of the board (prerequisite for diagonal victory)
             diagonal1 = [self.board[i] for i in [0,8,4]] #find content in left upper to right lower diagonal
             if all([spot == letter for spot in diagonal1]):
-                print('Winner by diagonal1')
+                #print('Winner by diagonal1')
                 return True
             diagonal2 = [self.board[i] for i in [6, 2, 4]]  # find content in left lower to right upper diagonal
             if all([spot == letter for spot in diagonal2]):
-                print('Winner by doaginal2')
+                #print('Winner by doaginal2')
                 return True
 
         #print('No winner yet')
@@ -84,14 +84,14 @@ def play(game, Xplayr, Oplayr, print_game=True) :#initiates & runs the entire th
                     print(letter+' wins!')
                 return letter #return letter that won
 
-            print('No winner yet')
+            #print('No winner yet')
             letter = 'O' if letter == 'X' else 'X'
 
     if print_game:
         print('It\'s a tie')
 
 if __name__ == '__main__':
-    Xplayr = HumPlay('X')
-    Oplayr = RandPC('Y')
+    Oplayr = HumPlay('O')
+    Xplayr = GeniPC('X')
     t = TicTacToe()
     play(t, Xplayr, Oplayr, print_game=True)
--- 12_beginner_projects_fCC/Tic_tac_toe_player.py
+++ 12_beginner_projects_fCC/Tic_tac_toe_player.py
@@ -34,3 +34,52 @@ class HumPlay(Player):
                 print('Invalid square. Try again.')
 
         return val
+
+class GeniPC(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.avail_moves()) == 9:
+            sqr = random.choice(game.avail_moves())
+        else:
+            sqr=self.minimax(game, self.letter)['position'] #only use position from best (position + score)
+            #needs to be recursive so define outside
+        return sqr
+
+    def minimax(self, state, player): #state=current state of the game like a screenshot in time
+        max_player = self.letter #not the Genius AI
+        other_player = 'O' if player == 'X' else 'X'
+
+        if state.winner == other_player: #check if previous move led to victory #
+            return {'position': None,
+                    'score': 1*(state.num_empty_sqrs() +1) if other_player == max_player #score should be maximised for other player (GeniPC)
+                            else -1*(state.num_empty_sqrs()+1) #score should be minimised for human
+                    }
+        elif not state.empty_sqrs(): #no empty squares, so nobody won, so score=0
+            return {'position' : None, 'score' : 0} #MiniMax returns the best position for the next move and the score for it
+
+        if player == max_player:
+            best = {'position' : None, 'score' : -math.inf} #initialise to negative infinity so that it'll raise when comparing with other score, ensuring making an update #best stores highest score and best position to move into. This has to be maximised every time
+        else :
+            best = {'position': None, 'score': math.inf} #should be minimised at every step so start with positive infinity
+
+        for possible_move in state.avail_moves(): #explore details of the next possible move
+            #make move, simulate & check impact of making that move, undo the move to come back to previous state of the game
+            #update details in dictionary if simulated move is better than previously recorded move
+            state.make_move(possible_move, player) #execute make_move for current state : assign letter, check winner
+
+            sim_score = self.minimax(state, other_player) #go into recursion, switch players and calculate. Again go into recursion, inception style
+
+            state.board[possible_move] = ' ' #undo data change from recursion
+            state.winner = None #undo data change from recursion
+            sim_score['position'] = possible_move #set simulated position to the move we evaluated in minimax
+
+            if player == max_player:
+                if sim_score['score'] > best['score']: # better outcome for max player
+                    best = sim_score #best & sim_score both deal with move position & score for that move
+            else:
+                if sim_score['score'] < best['score']: #better outcome for min player
+                    best = sim_score
+
+        return best #minimax returns best position & the score
